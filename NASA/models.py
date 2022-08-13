c624eca629f1d892fa50440a7b7ab1477df48a35
Andres <joseandressnchezflorez7722@gmail.com>
2022-08-12 20:46:50 -0500

    a

--- NASA/models.py
+++ NASA/models.py
@@ -13,7 +13,7 @@ class Usuario(models.Model):
     tipo_usuario = models.CharField(max_length=50)
     contrasena = models.CharField(max_length=50)
     estado = models.CharField(max_length=50)
-
+ 
 class Ventas_almacen(models.Model):
     id_ventas_a = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     id_zapatillas_a = models.IntegerField(null=True)
